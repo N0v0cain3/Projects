4ee639faa4a745e63d8506a350ba34ff77806b7b
Shivam Mehta <shivam.mehta2019@vitstudent.ac.in>
2020-07-06 16:13:48 +0530

    projects

---
+++ Backend/api/routes/project.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const multer = require("multer");
+const shortid = require("shortid");
+const nodemailer = require("nodemailer");
+const User = require("../models/user");
+const checkAuth = require("../middleware/checkAuth");
+const { getMaxListeners } = require("../models/user");
+require("dotenv").config();
